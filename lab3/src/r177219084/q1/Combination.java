4f453f9e343c92b2293f9d922791727057d21fa8
NEELAM PAREEK <ghpareek2002@gmail.com>
2021-03-01 18:10:42 +0530

    file that was left

--- lab3/src/r177219084/q1/Combination.java
+++ lab3/src/r177219084/q1/Combination.java
@@ -1,10 +1,27 @@
 package r177219084.q1;
 
+import java.util.Scanner;
+
 public class Combination {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a;
+		Scanner s = new Scanner(System.in);
+		int arr[] = new int[3];
+		for(int i = 0; i<3; i++)
+		{
+			a = s.nextInt();
+			arr[i] = a;
+		}
+		 for (int x = 0; x < 3; x++) {
+			   for (int y = 0; y < 3; y++) {
+			    for (int z = 0; z < 3; z++) {
+			     if (x != y && y != z && z != x) {
+			      System.out.println(arr[x] + "" + arr[y] + "" + arr[z]);
+			     }
+			    }
+			   }
 	}
-
+	}
 }
